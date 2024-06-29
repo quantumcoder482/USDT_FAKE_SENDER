94f7500f951297db020c67166f07ffc6c49073db
oliver1432 <github2@gamesbox.in>
2024-06-29 08:02:02 -0400

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+FOR USDTFLASHING
+Note : Our usdt flash demo is not free if you wanna send flash open software and pay gas fees that do flash upto for 1 between 20k.
+
+Contact us for Getting Software 
+Whatsapp : https://wa.me/16892782018
+Telegram : https://t.me/singhtechnologies
+
+For BTCFLASHING
+Note : our btc flashing demo free but only do flash at random address own address flash is in paid plans. Here btc flash software generated random address in demo program by this u can check urself and check how software works and you will know its real working
+
+
+Contact us for Getting Demo Software
+Whatsapp : https://wa.me/16892782018
+Telegram : https://t.me/singhtechnologies
